c2c8274a5945b323db71d9c932ee119c7c3afb0f
Erik Palencik <erikpa1@azet.sk>
2020-03-02 00:00:49 +0100

    Update C#

---
+++ SMSParser/SMSParser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using SMSParser;
+
+namespace SMSParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SMSParser parser = new SMSParser();
+
+            
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Ending because of no argument");
+                return;
+            }
+
+            Console.WriteLine("Args length: {0}", args[0]);
+
+            if (args.Length == 1)
+            {
+                Console.WriteLine(parser.Parse("HelloWorld!"));
+            }
+            else if (args.Length == 2)
+            {
+                if (args[0] == "-f")
+                {
+
+                }
+            }
+
+            
+            Console.WriteLine("App End");
+        }
+    }
+}
---
+++ SMSParser/SMSParser/SMSParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SMSParser
+{
+    class SMSParser
+    {
+        public SMSParser()
+        {
+
+        }
+
+        public String Parse(string stringToParse)
+        {
+            if (stringToParse == null)
+            {
+                Console.WriteLine("Not parsing because of null");
+                return "";
+            }
+            if (stringToParse == String.Empty)
+            {
+                Console.WriteLine("Nop parsing because of empty");
+                return "";
+            }
+
+            string concatedString = "";
+
+            foreach (char s in stringToParse)
+            {
+                if (concatedString.Length == 0)
+                {
+                    concatedString += s;
+                }
+                else
+                {
+                    if (Char.IsUpper(s))
+                    {
+                        concatedString += " ";
+                        concatedString += s;
+                    }
+                    else
+                    {
+                        concatedString += s;
+                    }
+                }           
+                               
+            }
+
+            return concatedString;
+            
+        }
+
+
+
+
+    }
+}
